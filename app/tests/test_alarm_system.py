0ddbc168fac1feef50449ca485ea3ec4dd151ee3
unknown <baptiste.poirier@efrei.net>
2023-04-11 20:53:23 +0200

    test: Test key_event function from AlarmSystem

--- app/tests/test_alarm_system.py
+++ app/tests/test_alarm_system.py
@@ -1,3 +1,6 @@
+from unittest.mock import MagicMock
+import keyboard
+
 from src.alarm_system import AlarmSystem
 from src.alarm import Alarm
 from src.alarm_type import AlarmType
@@ -8,10 +11,9 @@ def test_alarm_system_init_default() -> None:
 
     assert alarm_system.tick_rate == 0.25
 
-    alarm_low = Alarm(AlarmType.LOW, 0.25)
-    alarm_medium = Alarm(AlarmType.MEDIUM, 0.25)
-    alarm_high = Alarm(AlarmType.HIGH, 0.25)
-    alarms = [alarm_low, alarm_medium, alarm_high]
+    alarms = [Alarm(AlarmType.LOW, 0.25),
+              Alarm(AlarmType.MEDIUM, 0.25),
+              Alarm(AlarmType.HIGH, 0.25)]
 
     for i, alarm in enumerate(alarms):
         assert alarm == alarm_system.alarms[i]
@@ -22,9 +24,8 @@ def test_alarm_system_init() -> None:
 
     assert alarm_system.tick_rate == 0.125
 
-    alarm_low = Alarm(AlarmType.LOW, 0.125)
-    alarm_high = Alarm(AlarmType.HIGH, 0.125)
-    alarms = [alarm_low, alarm_high]
+    alarms = [Alarm(AlarmType.LOW, 0.125),
+              Alarm(AlarmType.HIGH, 0.125)]
 
     for i, alarm in enumerate(alarms):
         assert alarm == alarm_system.alarms[i]
@@ -49,3 +50,34 @@ def test_toggle_alarm() -> None:
     alarm_system.toggle_alarm(alarm_system.alarms[1])
     alarm_system.toggle_alarm(alarm_system.alarms[0])
     assert alarm_system.current_alarm_priority == 0
+
+
+def test_key_event() -> None:
+    alarm_system = AlarmSystem()
+
+    event = MagicMock(spec=keyboard._Event)
+    event.name = "l"
+    event.event_type = keyboard.KEY_DOWN
+
+    alarm_system.key_event(event)
+    assert alarm_system.alarms[0].active == True
+    assert "l" in alarm_system.pressed_keys
+
+    alarm_system.key_event(event)
+    assert alarm_system.alarms[0].active == True
+    assert "l" in alarm_system.pressed_keys
+
+    event.event_type = keyboard.KEY_UP
+    alarm_system.key_event(event)
+    assert alarm_system.alarms[0].active == True
+    assert "l" not in alarm_system.pressed_keys
+
+    event.event_type = keyboard.KEY_DOWN
+    alarm_system.key_event(event)
+    assert alarm_system.alarms[0].active == False
+    assert "l" in alarm_system.pressed_keys
+
+    event.name = "a"
+    alarm_system.key_event(event)
+    assert alarm_system.alarms[0].active == False
+    assert "a" not in alarm_system.pressed_keys
